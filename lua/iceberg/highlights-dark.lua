1f5d1d9c35a9d02cc71016699efcff27d37e56c2
jaatkinson <jaatkinson8@icloud.com>
2025-05-23 01:42:49 +0100

    More blink

--- lua/iceberg/highlights-dark.lua
+++ lua/iceberg/highlights-dark.lua
@@ -110,10 +110,10 @@ function M.set_highlights()
     hl(0, "FloatTitle", { fg = "#e2a578", bg = "NONE" })
 
 
-    hl(0, "BlinkCmpMenu", { bg = rgba(226,120,120, 0.7) })
+    hl(0, "BlinkCmpMenu", { link = "Normal" })
     hl(0, "BlinkCmpMenuBorder", { fg = "#3e4757", bg = "NONE" })
     hl(0, "BlinkCmpMenuSelection", { link = "CursorLine" })
-    -- hl(0, "BlinkCmpKind", {  })
+    hl(0, "BlinkCmpKind", { bg = "#161822", fg = "#e27878" })
 
     hl(0, "BlinkCmpDoc", { fg = "#c7c9d1", bg = "NONE" })
     hl(0, "BlinkCmpDocBorder", { fg = "#3e4757", bg = "NONE" })
